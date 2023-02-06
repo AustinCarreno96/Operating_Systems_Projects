9bd85e39964d2792b9c3f5cf4c8353cee042784b
AustinCarreno96 <austinraycarreno96@gmail.com>
2023-02-05 22:35:42 -0500

    Almost Finished With Second Part

--- Project_1/DOS.c
+++ Project_1/DOS.c
@@ -10,35 +10,39 @@ int main(int argc, char* argv[]) {
     char cmd[100];
     char* arg1;
     char* arg2;
+    char buffer[1024];
 
     int c = 0;
 
-    // Checking number of cmd args
-    // if(argc != 2) {
-    //     printf("Incorrect arguments given.");
-    //     exit(0);
-    // }
-    // cmd = "dir";
     while(flag) {
         printf("Type Ctrl-C to exit\n");
         scanf("%s", cmd);
+        char* token = strtok(cmd, " ");
 
         if(strcmp(cmd, "cd") == 0) {
             system("cd");
 
-        } else if (strcmp(cmd, "dir") == 0) {
+        } else if(strcmp(cmd, "dir") == 0) {
             system("ls");
 
-        } else if (strcmp(cmd, "type") == 0) {
+        } else if(strcmp(cmd, "type") == 0) {
             system("cat");
 
-        } else if (strcmp(cmd, "del") == 0) {
-            system("rm");
+        } else if(strcmp(cmd, "del") == 0) {
+            token = strtok(NULL, " ");
+            while(token) {
+            snprintf(buffer, sizeof(buffer), "rm %s", token);
+            token = strtok(NULL, " ");
+            }
+            printf("%s", buffer);
+            //printf("%s", token);
+            //snprintf(buffer, sizeof(buffer), "rm %s", token);
+            system(buffer);
 
-        } else if (strcmp(cmd, "ren") == 0) {
+        } else if(strcmp(cmd, "ren") == 0) {
             system("mv");
 
-        } else if (strcmp(cmd, "copy") == 0) {
+        } else if(strcmp(cmd, "copy") == 0) {
             system("cp");
 
         } else {
